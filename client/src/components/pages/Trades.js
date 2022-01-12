a80c0ef684a2df76368c32fc34f170c31e00ab94
Brady Sullivan <bsully@mit.edu>
2022-01-12 13:04:50 -0500

    Added tab functionality to Trades page

--- client/src/components/pages/Trades.js
+++ client/src/components/pages/Trades.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import Pending from "../modules/Pending.js";
 import Accepted from "../modules/Accepted.js";
 import Complete from "../modules/Complete.js";
@@ -12,12 +12,38 @@ import "./Trades.css";
 //  this will also likely need a state and modulo operator (0, 1, 2, 3, %3)
 
 const Trades = () => {
+  const [activeTab, setActiveTab] = useState("Pending");
+
+  const handlePending = () => setActiveTab("Pending");
+  const handleAccepted = () => setActiveTab("Accepted");
+  const handleComplete = () => setActiveTab("Complete");
+
   return (
     <div>
       <h1>Trades Page</h1>
-      <Pending />
-      <Accepted />
-      <Complete />
+      <div>
+        <button className={activeTab === "Pending" ? "active" : ""} onClick={handlePending}>
+          Pending
+        </button>
+        <button className={activeTab === "Accepted" ? "active" : ""} onClick={handleAccepted}>
+          Accepted
+        </button>
+        <button
+          className={activeTab === "Complete" ? "active" : "" + "u-bold"}
+          onClick={handleComplete}
+        >
+          Complete
+        </button>
+      </div>
+      <div className="outlet">
+        {activeTab === "Pending" ? (
+          <Pending />
+        ) : activeTab === "Accepted" ? (
+          <Accepted />
+        ) : (
+          <Complete />
+        )}
+      </div>
     </div>
   );
 };
