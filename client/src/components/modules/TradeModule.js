4e5624efc7298b8a75a7eb0840af46ff8a1f3d6f
Brady Sullivan <bsully@mit.edu>
2022-01-17 18:48:23 -0500

    last update before deploying this edition!

--- client/src/components/modules/TradeModule.js
+++ client/src/components/modules/TradeModule.js
@@ -247,7 +247,7 @@ export default TradeModule;
 //Sort into various pages
 //Create control for managing trades (approve/deny)
 //Create control for completing trades (switch/keep for self)
-//Create messaging system (socket)
+//Create messaging system (socket and messaging api!)
 
 //Create some initial styling!
 
